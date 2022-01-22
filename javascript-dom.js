0b2610064e24dc18d8668cf6de028a21987faa5a
PulexIrritans <pulex.irritans.hh@gmail.com>
2022-01-22 14:50:52 +0100

    Finished all processing based on user input

--- javascript-dom.js
+++ javascript-dom.js
@@ -1,32 +1,82 @@
-// Function to dynamically create payment forms
+// Global variables
 
 const orderedListElement = document.querySelector("ol");
 const submitGroupButton = document.getElementById("group-form-btn");
+const calculatePaymentsButton = document.getElementById("calculate-payments-btn");
+let groupSize = 0;
+let initialAveragePaid = 0;
+let nameArray = [];
+let amountArray = [];
+let payments = [];
+let payback =0;
 
 
-submitGroupButton.onclick = function() {
-    createPaymentForms(document.getElementById("groupsize").value) 
-}
+// DOM Manipulation
+// Helper function to set attributes to DOM elements
+
+function setAttributes(element, attributes) {
+    Object.keys(attributes).forEach(function(name) {
+      element.setAttribute(name, attributes[name]);
+    })
+  }
+
+// Function to dynamically create payment form elements (DOM) based on groupsize  
 
 function createPaymentForms(count) {
     
     for(let i=0; i<count; i++) {
         const newListElement = document.createElement("li");
         orderedListElement.append(newListElement);
+        newListElement.classList.add("box");
 
         const listElementn = document.getElementsByTagName("li")[i];
                 
         const newLabelNameElement = document.createElement("label");
+        setAttributes(newLabelNameElement, {
+            for: "name"
+          })
+          newLabelNameElement.innerText="Name #"+[i+1];
+
         const newInputNameElement = document.createElement("input");
+        setAttributes(newInputNameElement, {
+            type: "text",
+            id: "name",
+            name: "name"
+          })
+        
         const newLabelExpenseElement = document.createElement("label");
+        setAttributes(newLabelExpenseElement, {
+            for: "expense"
+          })
+          newLabelExpenseElement.innerText="Amount paid";
+
         const newInputExpenseElement = document.createElement("input");
+        setAttributes(newInputExpenseElement, {
+            type: "number",
+            id: "expense",
+            name: "expense"
+          })
         listElementn.append(newLabelNameElement);
         listElementn.append(newInputNameElement);
         listElementn.append(newLabelExpenseElement);
-        listElementn.append(newInputExpenseElement);
-
-    // Continue working here with some loop that will add all the attributes and styling to the elements.    
-    
+        listElementn.append(newInputExpenseElement);   
     };
-    // orderedListElement.setAttribute("style", "grid-template-columns: repeat("+axisLength+", 1fr); grid-template-rows: repeat("+axisLength+", 1fr)");
+
 };
+
+// Event Listeners
+
+submitGroupButton.onclick = function() {
+    orderedListElement.innerHTML = "";
+    groupSize = document.getElementById("groupsize").value;
+    createPaymentForms(groupSize);
+}
+
+calculatePaymentsButton.onclick = function() {
+    getGroupDetails(groupSize);
+    console.log(calculatePayments(groupSize));
+};
+
+
+
+
